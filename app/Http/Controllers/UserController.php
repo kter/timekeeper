54ac78435b80f6faabe01cc87e956a81fdac5c5e
kter <me@kter.jp>
2019-04-14 01:40:04 +0900

    コントローラーの中身の定義とroutesから呼べるようにする

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,5 +8,10 @@ use App\Http\Requests;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+
+        $query = \App\User::query();
+        $users = $query->orderBy('id', 'desc')->paginate(10);
+        return view('users.index')->with('users', $users);
+    }
 }
