db32350cde4fdb5f646d4970d26b7c92143aad8c
kter <me@kter.jp>
2019-04-14 01:23:13 +0900

    Usersのシーダーを追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,34 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->delete();
+
+        $faker = Faker\Factory::create('ja_JP');
+
+        for ($i = 0; $i < 8; $i++){
+            \App\User::create([
+                'name' => $faker->name(),
+                'email' => $faker->email()
+            ]);
+        }
+        \App\User::create([
+            'name' => $faker->name(),
+            'email' => $faker->email(),
+            'is_admin' => 1
+        ]);
+
+        \App\User::create([
+            'name' => $faker->name(),
+            'email' => $faker->email(),
+            'is_manager' => 1
+        ]);
+
+        \App\User::create([
+            'name' => $faker->name(),
+            'email' => $faker->email(),
+            'is_admin' => 1,
+            'is_manager' => 1
+        ]);
+
     }
 }
