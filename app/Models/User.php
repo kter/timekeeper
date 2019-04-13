57bc3989040d1a89546d03b1b2a8059db94046c2
kter <me@kter.jp>
2019-04-14 01:09:20 +0900

    セキュリティのためpasswordとremember_tokenを保護

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
 }
