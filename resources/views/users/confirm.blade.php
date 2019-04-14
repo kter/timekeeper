4e04b9b222952101152c8cb7789f2f63ec18c6c0
kter <me@kter.jp>
2019-04-14 20:51:25 +0900

    実データに合わせて修正

--- resources/views/users/confirm.blade.php
+++ resources/views/users/confirm.blade.php
@@ -2,13 +2,14 @@
 @section('title', 'Tutrial for beginner')
 @section('content')
     <div class="page-header" style="margin-top:-30px;padding-bottom:0px;">
-        <h1><small>社員登録</small></h1>
+        <h1><small>社員登録確認</small></h1>
     </div>
-    <form action="" method="post" class="form-horizontal">
+    <form action="create" method="post" class="form-horizontal">
         {{ csrf_field() }}
         <input type="hidden" name="name" value="{{$name}}">
         <input type="hidden" name="email" value="{{$email}}">
-        <input type="hidden" name="tel" value="{{$tel}}">
+        <input type="hidden" name="is_admin" value="{{$is_admin}}">
+        <input type="hidden" name="is_manager" value="{{$is_manager}}">
         <div class="row">
             <label class="col-sm-4 control-label">お名前</label>
             <div class="col-sm-8">{{$name}}</div>
@@ -18,8 +19,12 @@
             <div class="col-sm-8">{{$email}}</div>
         </div>
         <div class="row">
-            <label class="col-sm-4 control-label">電話番号</label>
-            <div class="col-sm-8">{{$tel}}</div>
+            <label class="col-sm-4 control-label">管理者フラグ</label>
+            <div class="col-sm-8">{{$is_admin}}</div>
+        </div>
+        <div class="row">
+            <label class="col-sm-4 control-label">マネージャーフラグ</label>
+            <div class="col-sm-8">{{$is_manager}}</div>
         </div>
         <div class="row" style="margin-top: 30px;">
             <div class="col-sm-offset-4 col-sm-8">
