76b5c294468d3087c55cfb4d91d712b1982e15fe
kter <me@kter.jp>
2019-04-14 20:50:59 +0900

    パスワードはとりあえずあと

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -26,7 +26,8 @@ class UserRequest extends Request
         return [
             'name' => 'required',
             'email' => 'required|email',
-            'password' => 'required'
+            // TODO: パスワードを入力させるようにする
+            // 'password' => 'required'
         ];
     }
 
