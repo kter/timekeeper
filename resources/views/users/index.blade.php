655b0608d21167f995d973ac5e623f33eb8f2086
kter <me@kter.jp>
2019-04-15 22:51:53 +0900

    一覧の各種フラグの表示改善

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,7 +19,23 @@
         <tbody>
         @foreach($users as $user)
             <tr>
-                <td>{{$user->id}}</td><td>{{$user->name}}</td><td>{{$user->email}}</td><td>{{$user->is_admin}}</td><td>{{$user->is_manager}}</td>
+                <td>{{$user->id}}</td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>
+                    @if ($user->is_admin)
+                        <div class="col-sm-8">ON</div>
+                    @else
+                        <div class="col-sm-8">OFF</div>
+                    @endif
+                </td>
+                <td>
+                    @if ($user->is_manager)
+                        <div class="col-sm-8">ON</div>
+                    @else
+                        <div class="col-sm-8">OFF</div>
+                    @endif
+                </td>
                 <td>
                     <a href="" class="btn btn-primary btn-sm">詳細</a>
                     <a href="" class="btn btn-primary btn-sm">編集</a>
