1679d252160c0cf2a6036340f7902d838331edbc
kter <me@kter.jp>
2019-04-20 19:06:11 +0900

    メールアドレス間違いのエラー

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 use App\Http\Requests;
+use Psr\Log\NullLogger;
 
 class LoginController extends Controller
 {
@@ -17,11 +19,13 @@ class LoginController extends Controller
 
     public function create(Request $request)
     {
-        \Log::info('ログ出力テスト');
-#        $user = User::where('email', $request->email);
-#        if (Hash::check($request->password, $user->password)) {
-#            \Log::info('ログ出力テスト');
-#        }
+        $user = User::where('email', $request->email)->first();
+        if (is_null($user)) {
+            return view('login.new')->withErrors(array('email' => '存在しないメールアドレスです。'));
+        }
+        if (Hash::check($request->password, $user->password)) {
+            \Log::info('ログ出力テスト');
+        }
     }
 
     public function destroy($message)
