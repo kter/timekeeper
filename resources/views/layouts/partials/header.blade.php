99df2d579c6e49f918bdfb7d51bdea69c8fce0a8
kter <me@kter.jp>
2019-04-21 19:35:58 +0900

    ログイン・ログアウトメニューの切替条件修正

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -21,18 +21,17 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth2::logged_in())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth2::current_user()->name }} <span class="caret"></span>
                         </a>
                         <ul class="dropdown-menu" role="menu">
-                            <li method="delete" name="form1" action="logout">
-                                <li><a href="javascript:form1.submit()"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </form>
-                        </ul>
+                                <li><form method="post" name="form1" action="logout"><input name="_method" type="hidden" value="DELETE">{{ csrf_field() }}<a href="javascript:form1.submit()"><i class="fa fa-btn fa-sign-out"></i>Logout</a></form></li>
+
+                       </ul>
                     </li>
+                @else
+                    <li><a href="{{ url('/login') }}">Login</a></li>
                 @endif
             </ul>
         </div>
