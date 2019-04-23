2edbb84f88f07ad75987b01ac83cc8f744fa0793
kter <me@kter.jp>
2019-04-23 09:19:53 +0900

    管理者かどうかのMiddlewareを追加

---
+++ app/Http/Middleware/OnlyForAdminMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Facades\Auth2;
+use Closure;
+
+class OnlyForAdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // TODO: これでいいのかな？
+        if ($request->session()->has('user_id')){
+            if (Auth2::is_admin('user_id')){
+                return $next($request);
+            }
+        }
+        return redirect('home');
+    }
+}
