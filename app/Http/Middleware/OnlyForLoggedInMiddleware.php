eaa3d20a05979797c98c31882a65f627b340db3d
kter <me@kter.jp>
2019-04-23 09:19:31 +0900

    ログインしているかどうかのMiddlewareを追加

--- app/Http/Middleware/OnlyForLoggedInMiddleware.php
+++ app/Http/Middleware/OnlyForLoggedInMiddleware.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class Auth2Middleware
+class OnlyForLoggedInMiddleware
 {
     /**
      * Handle an incoming request.
@@ -15,6 +15,10 @@ class Auth2Middleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        // TODO: これでいいのかな？
+        if ($request->session()->has('user_id')) {
+            return $next($request);
+        }
+        return redirect('home');
     }
 }
